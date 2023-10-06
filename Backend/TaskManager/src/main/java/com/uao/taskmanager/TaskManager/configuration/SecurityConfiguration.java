434f7a3be2e8c5340a75ae1f34b33e8c36b81b3d
Andres Soto <andres_f.soto@uao.edu.co>
2023-10-06 12:58:39 -0500

    task: solved issue about cors

--- Backend/TaskManager/src/main/java/com/uao/taskmanager/TaskManager/configuration/SecurityConfiguration.java
+++ Backend/TaskManager/src/main/java/com/uao/taskmanager/TaskManager/configuration/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.uao.taskmanager.TaskManager.configuration;
 
-import com.uao.taskmanager.TaskManager.configuration.JwtAuthenticationFilter;
 import com.uao.taskmanager.TaskManager.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,6 +22,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
+import static org.springframework.security.config.http.SessionCreationPolicy.ALWAYS;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
@@ -34,7 +35,8 @@ public class SecurityConfiguration {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf(AbstractHttpConfigurer::disable)
+    http.cors(Customizer.withDefaults())
+        .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(request -> request.requestMatchers("/v1/authentication/**")
             .permitAll().anyRequest().authenticated())
         .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
